c998f34eeef65ca008da4c03d429135344a1a209
ScathonLin <40821407+ScathonLin@users.noreply.github.com>
2018-12-01 17:42:26 +0800

    Create Configuration.java

---
+++ ML_Algorithm_Impl/LinearRegression/Configuration.java
@@ -0,0 +1,11 @@
+package com.huawei.scathon.aiops.logisticsregression;
+
+/**
+ * @author l00483433
+ */
+public class Configuration {
+    public static String LINE_SPLIT_SEPARATOR = ",";
+    public static Integer MAX_CYCLE_TIMES = 10000;
+    public static String DATA_FILE_NAME = "train_set.txt";
+    public static double LEARNING_STEP = 0.002;
+}

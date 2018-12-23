293622ec7943bc0a20d1fdab5aa3ac909ef4bf56
ScathonLin <40821407+ScathonLin@users.noreply.github.com>
2018-12-23 16:46:57 +0800

    Add files via upload

---
+++ Constants.java
@@ -0,0 +1,11 @@
+package com.huawei.scathon.aiops.utils;
+
+/**
+ * @author l00483433
+ */
+public class Constants {
+    public static final String SPACE_SEPARATOR = " ";
+    public static final String SPACE_SEPARATOR_NAME = "space";
+    public static final String TAB_SEPARATOR = "\t";
+    public static final String TAB_SEPARATOR_NAME = "tab";
+}
---
+++ Node.java
@@ -0,0 +1,49 @@
+package com.huawei.scathon.aiops.louvain.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+import lombok.experimental.Accessors;
+
+import java.util.Set;
+
+/**
+ * @author l00483433
+ */
+@AllArgsConstructor
+@NoArgsConstructor
+@Accessors(chain = true)
+@ToString
+@Builder
+public class Node {
+    @Getter @Setter private int nodeIndex;
+    // 社区环的权重，初始化的时候为0
+    @Getter @Setter private int ringWeight;
+    @Getter @Setter private Set<Edge> edges;
+
+    @Getter @Setter private Boolean isRemoved;
+    @Getter @Setter private int sigmaIn;
+    @Getter @Setter private int sigmaTot;
+    @Getter @Setter private int ki;
+
+    public Node(int nodeIndex) {
+        this.nodeIndex = nodeIndex;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj instanceof Node) {
+            Node target = (Node) obj;
+            return target.getNodeIndex() == nodeIndex;
+        }
+        return false;
+    }
+
+    @Override
+    public int hashCode() {
+        return String.valueOf(nodeIndex + "*#").hashCode();
+    }
+}
